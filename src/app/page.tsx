116597972547c1aa69d999783667485e1a425be6
LUISJG57 <a00836928@tec.mx>
2025-07-31 15:48:23 -0600

    insights2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Projects from "./(pages)/projects/page"
 import Contact from "./(pages)/contact/page"
 import Work from "./(pages)/experience/page"
 import Hero from "./(pages)/hero/page"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="">
+      <SpeedInsights />
       <div className="flex flex-col justify-center items-center min-h-screen">
         <section id="home">
           <Hero />
