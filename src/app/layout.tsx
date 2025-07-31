f5c1a9e027c6d158808f0bd1e460e99b471cf673
LUISJG57 <a00836928@tec.mx>
2025-07-31 15:43:18 -0600

    vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import Navbar from "./_components/navbar/navbar"
 import ObserverProvider from "./_components/ObserverProvider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <ObserverProvider>
           <main className="pt-1">
             {children}
+            <SpeedInsights />
           </main>
         </ObserverProvider>
       </body>
